c549f7cc6684b4daf03932bc2e7e6fdb6f5789a1
martin <martin.hermant@gmail.com>
2014-02-20 11:55:31 +0100

    myAlgos

---
+++ src/algorithms/standard/waveshaper.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2006-2013  Music Technology Group - Universitat Pompeu Fabra
+ *
+ * This file is part of Essentia
+ *
+ * Essentia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+#include "waveshaper.h"
+#include "essentiamath.h"
+
+using namespace std;
+using namespace essentia;
+using namespace standard;
+
+
+
+
+
+const char* waveshaper::name = "waveshaper";
+const char* waveshaper::description = DOC(
+"shapes the signal to a waveshaping model made of 3 continuous linear part\n"
+"(p1,p2),(p2,p3),(p3,p4) "
+"\n"
+"\n"
+"References:\n"
+" \n");
+
+
+void waveshaper::configure() {
+
+_isAbs = parameter("isAbs").toBool();
+_isRel = parameter("isRel").toBool();
+
+  _p1x =  parameter("p1x").toReal();
+  _p2x =  parameter("p2x").toReal();
+  _p3x =  parameter("p3x").toReal();
+  _p4x =  parameter("p4x").toReal();
+  _p1y =  parameter("p1y").toReal();
+  _p2y =  parameter("p2y").toReal();
+  _p3y =  parameter("p3y").toReal();
+  _p4y =  parameter("p4y").toReal();
+if(_p1x>=_p2x||_p2x>=_p3x||_p3x>=_p4x) throw EssentiaException("wrong Params or badly sorted p1x<p2x<p3x<p4x") ;
+  _c1 = (_p2y-_p1y)*1.0/(_p2x-_p1x);
+  _c2 = (_p3y-_p2y)*1.0/(_p3x-_p2x);
+  _c3 = (_p4y-_p3y)*1.0/(_p4x-_p3x);
+  
+  
+
+}
+
+void waveshaper::compute() {
+
+  const std::vector<Real>& signal = _signal.get();
+  vector<Real>& signalout = _signalout.get();
+	
+  if (int(signal.size()) <= 1) {
+    signalout.resize(0);
+    throw EssentiaException("nosignal");
+    return;
+  }
+  else{
+  signalout.resize( int(signal.size()));
+  float max =1.0;
+  if (_isRel){
+  max = 0.000001;
+  for (int i = 0;i<int(signal.size());i++){
+  max = std::max(max,abs(signal[i]));
+  }
+  }
+  bool neg = false;
+  for(int i = 0 ; i< int(signal.size());i++){
+  Real cur = signal[i];
+  if(_isRel){cur/=max;}
+  if(_isAbs){neg =cur<0; cur = abs(cur);} 
+	if(cur>_p1x&&cur<=_p2x)  
+		{signalout[i] = (Real)(_p1y+(cur-_p1x)*_c1);}
+	else if(cur>_p2x&&cur<=_p3x) 
+		{signalout[i] = (Real)(_p2y+(cur-_p2x)*_c2);}
+	else if(cur>_p3x&&cur<=_p4x) 
+		{signalout[i] = (Real)(_p3y+(cur-_p3x)*_c3);}
+	else if(cur<_p1x) 
+		{signalout[i] = (Real)_p1y;}
+	else 
+		{signalout[i] = (Real)_p4y;}
+   if(_isAbs && neg){signalout[i] *=-1;}
+  }
+  
+  }
+
+
+
+  
+
+}
+
---
+++ src/algorithms/standard/waveshaper.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2006-2013  Music Technology Group - Universitat Pompeu Fabra
+ *
+ * This file is part of Essentia
+ *
+ * Essentia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+#ifndef ESSENTIA_waveshaper_H
+#define ESSENTIA_waveshaper_H
+
+#include "algorithmfactory.h"
+#include <complex>
+
+namespace essentia {
+namespace standard {
+
+class waveshaper : public Algorithm {
+
+ private:
+  Input<std::vector<Real> > _signal;
+  Output<std::vector<Real> > _signalout;
+
+
+
+ public:
+  waveshaper()   {
+    declareInput(_signal, "array", "the array to be shaped");
+    declareOutput(_signalout, "shaped_signal", "shaped");
+
+  }
+
+  void declareParameters() {
+
+	declareParameter("p1x","coordinate","(-inf,inf)",0);
+	declareParameter("p1y","coordinate","(-inf,inf)",0);
+	declareParameter("p2x","coordinate","(-inf,inf)",0.3);
+	declareParameter("p2y","coordinate","(-inf,inf)",0.3);
+	declareParameter("p3x","coordinate","(-inf,inf)",0.6);
+	declareParameter("p3y","coordinate","(-inf,inf)",0.6);
+	declareParameter("p4x","coordinate","(-inf,inf)",1);
+	declareParameter("p4y","coordinate","(-inf,inf)",1);
+	declareParameter("isAbs","symetric mode","{true,false}", true);
+	declareParameter("isRel","normalisation mode","{true,false}", true);
+  }
+
+  void configure();
+  void compute();
+
+
+  static const char* name;
+  static const char* description;
+  
+  
+  private:
+	Real _p1x;
+	Real  _p2x;
+	Real  _p3x;
+	Real  _p4x;
+	Real  _p1y;
+	Real  _p2y;
+	Real  _p3y;
+	Real  _p4y;
+	double _c1;
+	double _c2;
+	double _c3;
+	bool _isAbs;
+	bool _isRel;
+
+};
+
+} // namespace standard
+} // namespace essentia
+
+#include "streamingalgorithmwrapper.h"
+
+namespace essentia {
+namespace streaming {
+
+class waveshaper : public StreamingAlgorithmWrapper {
+
+ protected:
+  Sink<std::vector<Real> > _signal;
+  Source<std::vector<Real> > _signalout;
+
+ public:
+  waveshaper() {
+    declareAlgorithm("waveshaper");
+    declareInput(_signal, TOKEN, "array");
+    declareOutput(_signalout, TOKEN, "shaped_signal");
+  }
+};
+
+} // namespace streaming
+} // namespace essentia
+
+#endif // ESSENTIA_AUTOCORRELATION_H
